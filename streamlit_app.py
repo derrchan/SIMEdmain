39d09d8f7d0063d55a43b22a5a68e5f34d3e0d55
derrchan <141563495+derrchan@users.noreply.github.com>
2023-11-21 19:49:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     # Main page title
-    st.title('HKUMed Surgery Tutor Apps')
+    st.title('HKUMed Surgery Tutor Apps 🩺🔪')
 
     # Use markdown to create hyperlinks to the deployed Streamlit apps
     st.markdown('Below are the HKUMed Surgery Tutor apps available:')
@@ -11,7 +11,7 @@ def main():
     st.markdown('[Breast Surgery MCQs](https://simedmcq-nywtfsvzjpgvwnowpzvvgf.streamlit.app/)')
 
     # Link to the deployed saq.py app
-    st.markdown('[Tutor Bot](https://share.streamlit.io/yourusername/yourrepository/main/saq.py)')
+    st.markdown('[Surgery Tutor Bot 🔪](https://share.streamlit.io/yourusername/yourrepository/main/saq.py)')
 
 # Check if the script is being run directly
 if __name__ == '__main__':
